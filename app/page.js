325ff0a23416be5c0667cd74db12f6c3ce04a949
Anuj Patel <ap@Anujs-MacBook-Air.local>
2024-10-30 12:33:00 +0530

    resolve conf

--- app/page.js
+++ app/page.js
@@ -1,30 +1,24 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
-<<<<<<< Updated upstream
 import AchievementSection from "./components/AchievementSection";
-
-=======
-import ContactUs from "./components/ContactUs";
 import Testimonial from "./components/Testimonial";
 import Brands from "./components/Brands";
->>>>>>> Stashed changes
+import ContactUs from "./components/ContactUs";
+
+
 export default function Home() {
   return (
     <div className="w-screen ">
       <Navbar/>
-<<<<<<< Updated upstream
       <HeroSection/>
       <AchievementSection/>
       <Footer/>
-    </>
-=======
       <HeroSection />
       <Testimonial/>
       <Brands />
       <ContactUs />
       <Footer />
     </div>
->>>>>>> Stashed changes
   );
 }
