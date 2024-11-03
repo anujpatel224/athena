2e131c2c1bedd4db981ecbde1a14919bbe10ade0
Rahul vats <rahulvats166@gmail.com>
2024-11-03 16:44:54 +0530

    layout fix

--- app/page.js
+++ app/page.js
@@ -1,17 +1,21 @@
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
 import AchievementSection from "./components/AchievementSection";
+import OurServices from "./components/OurServices";
+import LiveEvents from "./components/LiveEvents";
 import Testimonial from "./components/Testimonial";
 import Brands from "./components/Brands";
 import ContactUs from "./components/ContactUs";
-
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <div className="w-screen ">
-      <Navbar/>
-      <HeroSection/>
-      <AchievementSection/>
+    <div className="scroll-container h-screen w-screen">
+      <Navbar />
+      <HeroSection />
+      <AchievementSection />
+      <OurServices />
+      <LiveEvents/>
       <Testimonial/>
       <Brands />
       <ContactUs />
