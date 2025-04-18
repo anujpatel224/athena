ba6241486c7c55eceeee58d7435074755733179c
Rahul vats <rahulvats166@gmail.com>
2025-04-19 01:16:35 +0530

    insight pages push

--- js/insight.js
+++ js/insight.js
@@ -78,3 +78,16 @@ function adjustBgHeight() {
 
 window.addEventListener("load", adjustBgHeight);
 window.addEventListener("resize", adjustBgHeight);
+
+const slider = document.getElementById('slider');
+const leftArrow = document.querySelector('.arrow.left');
+const rightArrow = document.querySelector('.arrow.right');
+
+leftArrow.addEventListener('click', () => {
+  slider.scrollBy({ left: -300, behavior: 'smooth' });
+});
+
+rightArrow.addEventListener('click', () => {
+  slider.scrollBy({ left: 300, behavior: 'smooth' });
+});
+
